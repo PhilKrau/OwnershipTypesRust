bb6c0b71352d0a6fe9af703c4166585c93364e1b
PhilKrau <Phil.Krause1@gmail.com>
2024-04-27 12:54:02 +0200

    better comments

--- Beispiel1.rs
+++ Beispiel1.rs
@@ -9,21 +9,21 @@ fn main(){
     
     let s1_clone = s1.clone(); // Kopie des Speicherbereichs von "Hello World!" wird angelegt
     println!("s1 -> {}, s1_clone -> {}", s1, s1_clone);
-    let s2 = s1; // s2 -> "Hello World" s1 ungültig
+    let s2 = s1; // s2 -> "String1" s1 ungültig
     // Ownership des Heap Speicherbereichs wurde auf s2 übertragen
 
     println!("s2 -> {}", s2);
     take_ownership(s2); // Ownership wird an Methode übertragen
-    // println!("{}", s2); // Funktioniert nicht da s2 nicht mehr Owner ist
+    // println!("s2-> {}", s2); // Funktioniert nicht da s2 nicht mehr Owner ist
    
     let s3 = take_ownership_and_give_Back(s1_clone);
     println!("s3 -> {}", s3);
 
-} // Stack wird freigegeben. Speicher auf den s2 zeigt wird freigegeben da s2 out of scope
+} // Stack wird freigegeben. Speicher auf den s2 und s3 zeigen wird freigegeben
 
 fn take_ownership(string: String){ // 
     println!("take_ownership string -> {}", string);
-} // string out of scope --> Speicher wird freigegeben
+} // string Variable out of scope --> Speicher wird freigegeben
 
 fn take_ownership_and_give_Back(string: String) -> String {
     println!("take_ownership_and_give_back string -> {}", string);
