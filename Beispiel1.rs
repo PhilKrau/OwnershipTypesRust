f9bbb70aea39043996d6e6dce8bfef565dfe5f62
PhilKrau <Phil.Krause1@gmail.com>
2024-05-02 14:18:48 +0200

    added c++ comparison

--- Beispiel1.rs
+++ Beispiel1.rs
@@ -7,16 +7,20 @@ fn main(){
     // s1 ist Pointer zu in Heap angelegtem String Object
     let s1 = String::from("String1"); // s1 -> "Hello World"    
     
-    let s1_clone = s1.clone(); // Kopie des Speicherbereichs von "Hello World!" wird angelegt
-    println!("s1 -> {}, s1_clone -> {}", s1, s1_clone);
     let s2 = s1; // s2 -> "String1" s1 ungültig
     // Ownership des Heap Speicherbereichs wurde auf s2 übertragen
-
+    // C++
+    // string* s1 = "String1";
+    // string* s2 = s1;
+    // s1 = nullptr;
     println!("s2 -> {}", s2);
+    let s2_clone = s2.clone(); // Kopie des Speicherbereichs von "Hello World!" wird angelegt
+    println!("s2 -> {}, s2_clone -> {}", s2, s2_clone);
+
     take_ownership(s2); // Ownership wird an Methode übertragen
     // println!("s2-> {}", s2); // Funktioniert nicht da s2 nicht mehr Owner ist
    
-    let s3 = take_ownership_and_give_Back(s1_clone);
+    let s3 = take_ownership_and_give_back(s2_clone);
     println!("s3 -> {}", s3);
 
 } // Stack wird freigegeben. Speicher auf den s2 und s3 zeigen wird freigegeben
@@ -25,7 +29,7 @@ fn take_ownership(string: String){ //
     println!("take_ownership string -> {}", string);
 } // string Variable out of scope --> Speicher wird freigegeben
 
-fn take_ownership_and_give_Back(string: String) -> String {
+fn take_ownership_and_give_back(string: String) -> String {
     println!("take_ownership_and_give_back string -> {}", string);
     string // Return Wert gibt Ownership zurück
 }
